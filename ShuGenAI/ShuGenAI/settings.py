0c3beadf25d3bb5ca3fcc7da87392c8ce9c17330
Andrian <mr.aleryp@gmail.com>
2024-11-21 19:00:42 +0200

    Allow all hosts.

--- ShuGenAI/ShuGenAI/settings.py
+++ ShuGenAI/ShuGenAI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DJANGO_DEBUG", "True") == "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['*']
 
 
 # Application definition
